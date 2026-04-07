be82a666c30a5210c78ff59c3a2dec2bd68a5e18
VenkatRam2002 <komminenivenkatram@gmail.com>
2026-04-07 10:59:31 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_result_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password_here"; // Change this
+    private static final String PASSWORD = "Ram"; // Change this
 
     private static Connection connection = null;
 
